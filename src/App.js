fd790dab24d702f395e9912b753d0c5cff9d3cbb
Mostafiz-riya <rimostafiz99@gmail.com>
2024-05-09 02:47:43 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialItems = [
+    { id: 1, description: "Passports🛂", quantity: 2, packed: false },
+    { id: 2, description: "sunglasses🕶️", quantity: 12, packed: false },
+    { id: 3, description: "charger📴", quantity: 5, packed: true },
+  ];
+
+export default function App(){
+return(
+<div className="app">
+    <Logo/>
+    <Form/>
+    <Lists/>
+    <Stats/>
+     </div>
+);
 }
-
-export default App;
+function Logo(){
+    return(
+        <div>
+            <h1>🌻Far Away💦</h1>
+        </div>
+    );
+}
+function Form(){
+    const [description,setDescription]=useState("");
+    const[quantity,setQuantity]=useState(3);
+    // page refresh hobena,single dynamic page
+    function handleSubmit(e){
+        e.preventdefault()
+    }
+    return(
+        <form className="add-form" onSubmit={handleSubmit}>
+            <h3>what is needed for the trip?🤠</h3>
+            <select
+            value={quantity}
+            onChange={(e)=>setQuantity(e.target.value)}>
+            {Array.from({length:20},(_,i)=>i+1)
+            .map((num)=>(
+                 <option value={num} key={num}>
+                    {num}
+                </option>))}
+            </select>
+            <input type="text" 
+            placeholder="item name"
+            value={description}
+            onChange={(e)=>setDescription(e.target.value)}
+            />
+            <button>Add</button>
+        </form>
+    );
+}
+function  Lists(){
+    return (
+            
+        <div className="list">
+            <ul>
+            {initialItems.map(item=><Items item={item}/>)}
+            </ul>
+        </div>
+    );
+}
+// child jsx for Items component
+function Items({item}){
+    return(
+<span style={item.packed?{textDecoration:"Line-through"}:{}}>
+    <li>
+        {item.quantity} {item.description}
+        <button>❎</button>     
+    </li>
+</span>
+   );
+}
+function Stats(){
+    return(
+        <footer className="stats">
+<em>you have X items on your list and already packed X(%X)</em>
+        </footer>
+    );
+}
